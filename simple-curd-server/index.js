b48d9b551468d58be92067cbc094f190ef23101f
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-07 22:23:44 +0600

    New server created with mongodb

---
+++ simple-curd-server/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// mdyeasinrafi7
+// LBc6jWOFCAXdF10Q
+
+const uri =
+  "mongodb+srv://mdyeasinrafi7:LBc6jWOFCAXdF10Q@cluster0.p4ps1ct.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("CURD application in running");
+});
+
+app.listen(port, () => {
+  console.log(`CURD application running on port ${port}`);
+});
