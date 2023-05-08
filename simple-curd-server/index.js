75646d9283714bee369d4e12e6557332621196f7
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-08 23:13:56 +0600

    Getting data from mongo

--- simple-curd-server/index.js
+++ simple-curd-server/index.js
@@ -30,8 +30,13 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
-    const database = client.db("usersDB");
-    const userCollection = database.collection("users");
+    const userCollection = client.db("usersDB").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
