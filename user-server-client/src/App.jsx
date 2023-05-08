624e602a6110eac7615fee0e8a14b76893855546
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-08 22:36:36 +0600

    Uploading json

--- user-server-client/src/App.jsx
+++ user-server-client/src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <>
-      <h1>Simpl CURD Application</h1>
+      <h1>Simple CURD Application</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add" />
+      </form>
     </>
   );
 }
