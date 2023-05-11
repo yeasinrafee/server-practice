8b5226a2e3490e558fe506e6ae8ccab83ec70b99
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-11 08:54:29 +0600

    Deleted function added

--- user-server-client/src/components/Users.jsx
+++ user-server-client/src/components/Users.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUser] = useState(loadedUsers);
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("Deleted");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUser(remaining);
+        }
+      });
+  };
   return (
     <div>
       <h1>Users: {users.length}</h1>
       <div>
         {users.map((user) => (
           <p key={user._id}>
-            {user.name}: {user.email}
+            {user.name}: {user.email}{" "}
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
