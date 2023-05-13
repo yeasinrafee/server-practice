f6b185daa667b493c304d384958f737a6f767235
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-13 15:17:31 +0600

    Environment variable added for db user and pass

--- Coffee-site/coffee-site-server/index.js
+++ Coffee-site/coffee-site-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// 15ratWWC7pJXMVzk
-
-const uri =
-  "mongodb+srv://coffeeMaster:15ratWWC7pJXMVzk@cluster0.p4ps1ct.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p4ps1ct.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
