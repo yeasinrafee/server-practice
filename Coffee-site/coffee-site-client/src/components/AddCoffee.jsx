dca341c05ac49c96ce4189e46a0ed5bdd13a98cd
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-13 16:03:18 +0600

    Getting all input data from the form

--- Coffee-site/coffee-site-client/src/components/AddCoffee.jsx
+++ Coffee-site/coffee-site-client/src/components/AddCoffee.jsx
@@ -1,8 +1,31 @@
 const AddCoffee = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+    console.log(newCoffee);
+  };
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h1 className="text-3xl font-extrabold">Add Coffee</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* Row for Name and Category  */}
         <div className="flex gap-6 mb-8">
           <div className="form-control md:w-1/2">
@@ -12,6 +35,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="name"
                 placeholder="Enter Coffee Name"
                 className="input input-bordered w-full"
               />
@@ -24,6 +48,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="quantity"
                 placeholder="Available Quantity"
                 className="input input-bordered w-full"
               />
@@ -39,6 +64,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="supplier"
                 placeholder="Enter Supplier Name"
                 className="input input-bordered w-full"
               />
@@ -51,6 +77,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="taste"
                 placeholder="Taste"
                 className="input input-bordered w-full"
               />
@@ -66,6 +93,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="category"
                 placeholder="Category"
                 className="input input-bordered w-full"
               />
@@ -78,6 +106,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="details"
                 placeholder="Details"
                 className="input input-bordered w-full"
               />
@@ -93,6 +122,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
+                name="photo"
                 placeholder="Photo URL"
                 className="input input-bordered w-full"
               />
