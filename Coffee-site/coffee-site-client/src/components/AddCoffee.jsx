86f4ff084acb5214de843fe011b099169bf3bc59
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-13 15:52:15 +0600

    Add coffee input form

---
+++ Coffee-site/coffee-site-client/src/components/AddCoffee.jsx
@@ -0,0 +1,108 @@
+const AddCoffee = () => {
+  return (
+    <div className="bg-[#F4F3F0] p-24">
+      <h1 className="text-3xl font-extrabold">Add Coffee</h1>
+      <form>
+        {/* Row for Name and Category  */}
+        <div className="flex gap-6 mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Enter Coffee Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Row for Supplier and taste  */}
+        <div className="flex gap-6 mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Supplier Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Enter Supplier Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Taste</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Taste"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Row for Name and Category  */}
+        <div className="flex gap-6 mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Category"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Details</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Details"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Row for Name and Category  */}
+        <div className="flex gap-6 mb-8">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Photo URL"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <input type="submit" value="Add Coffee" className="btn btn-block" />
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
--- Coffee-site/coffee-site-client/src/main.jsx
+++ Coffee-site/coffee-site-client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "addCoffee",
+    element: <AddCoffee />,
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffee />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
